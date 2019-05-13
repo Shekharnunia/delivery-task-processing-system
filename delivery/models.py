55091b82977d497281e1e81733b84c7242d9fdba
Shekhar <shekharnunia@gmail.com>
2019-05-13 21:49:55 +0530

    states field also added into models and applied migrations to tables

--- delivery/models.py
+++ delivery/models.py
@@ -11,7 +11,20 @@ class DeliveryTask(models.Model):
         (MEDIUM, ("Medium")),
         (HIGH, ("High")),
     )
+    NEW = "NW"
+    ACCEPTED = "AC"
+    COMPLETED = "CM"
+    DECLINED = "D"
+    CANCELLED = "CN"
+    STATES = (
+        (NEW, ("New")),
+        (ACCEPTED, ("Accepted")),
+        (COMPLETED, ("Completed")),
+        (DECLINED, ("Declined")),
+        (CANCELLED, ("Cancelled")),
+    )
     title = models.CharField(max_length=150)
     creation = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     priority = models.CharField(choices=PRIORITIES, default=LOW, max_length=8)
+    states = models.CharField(choices=STATES, default=NEW, max_length=9)
